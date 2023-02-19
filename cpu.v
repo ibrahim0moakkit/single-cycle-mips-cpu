6996ac72dabfa1fa5fd126f36630847e88d24117
Ibrahim Moakkit <108411357+ibrahim0moakkit@users.noreply.github.com>
2023-02-19 16:40:04 +0300

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -12,49 +12,6 @@
 //datapath module consists of program counter, register file, alu modules
 
 
-`timescale 1ns/1ns
-module testbench();
-reg clk;
-reg reset;
-wire [31:0] writedata, dataadr;
-wire memwrite;
-
-// instantiate module to be tested
-top dut (clk, reset, writedata, dataadr, memwrite);
-
-// initialize test
-initial
-begin
-reset <= 1; # 22; reset <= 0;
-end
-
-// clock
-always
-begin
-clk <= 1; # 5; clk <= 0; # 5;
-end
-
-initial begin
-   $dumpfile("testbench.vcd");
-   $dumpvars(0, testbench);
-end
-
-// check results
-always @ (negedge clk)
-begin
-   if (memwrite) begin
-      if (dataadr == 84 && writedata == 7) begin
-          $display ("Simulation succeeded");
-             $stop;
-                end 
-                   else if (dataadr != 80) begin
-                       $display ("Simulation failed");
-                           $stop;
-                       end
-                   end
-               end
-    endmodule
-
 
 module alu
 (
